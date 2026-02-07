e805ea22e50159695c69493a87c30ff672a4049e
Robert Berry <rob@robertlberry.com>
2026-02-07 14:25:09 -0500

    Fixed some variable errors.

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ module "networking_setup" {
   # Definitions
   environment         = var.environment
   vpc_region          = "us-east-1"
-  vpc_cidr_block      = var.environment == "prod" ? var.prod_cidr : var.dev_cidr
+  vpc_cidr_block      = var.dev_cidr
   public_subnet_cidr  = "10.10.1.0/24"
   private_subnet_cidr = "10.10.2.0/24"
   az_public_subnet    = local.public_subnet_az
@@ -41,7 +41,7 @@ module "security_setup" {
 
   # Definitions
   vpc_id         = module.networking_setup.vpc_id
-  external_cidrs = [var.home_ip]
+  external_cidrs = var.allowed_external_cidrs
   # stick with defaults defined in security module
 }
 
--- variables.tf
+++ variables.tf
@@ -8,23 +8,18 @@ variable "environment" {
   }
 }
 
-variable "prod_cidr" {
-  description = "The CIDR used for AWS production VPC."
-  type        = string
-}
+#variable "prod_cidr" {
+#  description = "The CIDR used for AWS production VPC."
+#  type        = string
+#}
 
-variable "staging_cidr" {
-  description = "The CIDR used for AWS staging VPC."
-  type        = string
-}
+#variable "staging_cidr" {
+#  description = "The CIDR used for AWS staging VPC."
+#  type        = string
+#}
 
 variable "dev_cidr" {
   description = "The CIDR used for AWS development VPC."
-  type        = string
-}
-
-variable "home_ip" {
-  description = "My home ip address."
   type        = string
 }
 
@@ -32,4 +27,5 @@ variable "allowed_external_cidrs" {
   description = "External CIDRs allowed to ssh into the VPC."
   type        = list(string)
 }
+
 
